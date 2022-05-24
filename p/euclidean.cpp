03c4868ffb46ac2d8eef039a8b364445b49cb1e7
junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2022-05-24 17:17:19 +0900

    Add files via upload

---
+++ p/euclidean.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int euclidean(int a, int b);
+
+int main() {
+	int a, b, c;
+	printf("세 수를 입력하시오 : ");
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	scanf_s("%d", & c);
+
+	printf("세 수의 최대공약수는 : %d", euclidean(c, euclidean(a, b)));
+
+	return 0;
+}
+
+int euclidean(int a, int b) {
+	int r = a % b;
+	if (r == 0)
+		return b;
+	else
+		return euclidean(b, r);
+}
