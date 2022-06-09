a4d509c05025649d3267cf926776ad4afb9502fa
junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2022-06-09 14:02:45 +0900

    Add files via upload

---
+++ practice/find_location.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int find_loca(char* str, char* word);
+
+int main() {
+	char str[100];
+	char word[100];
+
+	printf("put a sentence \n");
+	scanf("%s", str);
+
+	printf("put a word \n");
+	scanf("%s", word);
+
+	find_loca(str, word);
+
+	return 0;
+}
+
+int find_loca(char* str, char* word) {
+	int loca=0, temp=0;
+	while (*str) {
+		if (*str == *word) {
+			while (*str) {
+				if (*str == *word) {
+					str++;
+					word++;
+					temp++;
+				}
+				else if(*word == '\0') {
+					printf("location is %d.", loca);
+					return 0;
+					
+				}
+				else {
+					loca = loca + temp;
+					word = word - temp;
+					temp = 0;
+					break;
+				}
+			}
+		}
+		else
+		str++;
+		loca++;
+	}
+
+	if (*str == '\0') {
+		printf("Error!!! check your word RIGHT NOW!!!");
+	}
+}
